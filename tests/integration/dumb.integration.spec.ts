0e42cdaa6dd62fd2d311cc1aecaf5623e9a3f490
floriansollami <43452332+floriansollami@users.noreply.github.com>
2025-01-13 18:37:24 +0100

    chore: use apache/kafka-native:latest for integration tests

--- tests/integration/dumb.integration.spec.ts
+++ tests/integration/dumb.integration.spec.ts
@@ -93,7 +93,54 @@ describe('dumb', () => {
     await kafkaTestHelper.cleanUp();
   });
 
-  it('should', () => {
+  it('should ', () => {
     expect(1 + 1).toBe(2);
   });
+
+  //   // TESTER LE BATCH EN ERREUR AVEC LES EVENT EMITTER
+  // // 1. Produce messages (one that will cause an error)
+  // const testTopic = "test.batch.topic";
+  // await produceMessages(testTopic, [
+  //   { key: "ok-1", value: JSON.stringify({ msg: "good-1" }) },
+  //   { key: "ok-2", value: JSON.stringify({ msg: "good-2" }) },
+  //   { key: "FAIL", value: JSON.stringify({ msg: "will-fail" }) },
+  //   { key: "ok-3", value: JSON.stringify({ msg: "good-3" }) },
+  // ]);
+
+  // // 2. Wait until we detect that all messages have been processed
+  // //    (or we time out). For example, we might expect 3 successes + 1 error.
+  // await waitForCondition(() => {
+  //   // dans notre cas ce serait 3 republish dans un nouveau topic
+  //   // + 1 dans le error handler
+  //   return consumedSuccesses.length + consumedErrors.length >= 4;
+  // }, 10000); // wait up to 10s
+
+  // // 3. Assertions on the *real* side effects:
+  // //    - 3 "success" messages consumed
+  // //    - 1 "error" message consumed
+  // expect(consumedSuccesses.length).toBe(3);
+  // expect(consumedErrors.length).toBe(1);
+
+  // // 4. Check that the same failing message is not re-processed
+  // //    We'll wait a little longer to ensure Kafka doesn't redeliver.
+  // await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  // test('should consume all messages from the topic in test 1', async () => {
+  //   const messages = ['message1', 'message2'];
+
+  //   await kafkaHelper.produce(messages);
+  //   await kafkaHelper.waitForMessages(messagesToProduce.length);
+
+  //   expect(kafkaHelper.getMessages().map((msg) => msg.value.toString())).toEqual(messagesToProduce);
+  // });
+
+  // test('should consume all messages from the topic in test 2', async () => {
+  //   const messages = ['message3', 'message4', 'message5'];
+  //   await kafkaHelper.produce(messagesToProduce);
+
+  //   await kafkaHelper.waitForMessages(messagesToProduce.length);
+
+  //   const messages = kafkaHelper.getMessages();
+  //   expect(messages.map((msg) => msg.value.toString())).toEqual(messagesToProduce);
+  // });
 });
--- tests/utils/kafkajs-test-helper.ts
+++ tests/utils/kafkajs-test-helper.ts
@@ -8,6 +8,8 @@ import {
 import { schema } from 'avsc';
 import axios, { type AxiosInstance } from 'axios';
 import { type Admin, Consumer, type ITopicConfig, Kafka, KafkaMessage, logLevel, Producer } from 'kafkajs';
+import { expect } from 'vitest';
+import waitForExpect from 'wait-for-expect';
 
 /*
 | Behavior           | TopicNameStrategy                                 | RecordNameStrategy           | TopicRecordNameStrategy                    |
@@ -55,7 +57,7 @@ export class KafkajsTestHelper {
   private producer: Producer | null = null;
 
   // Keep track of consumed messages and the latest offset
-  private readonly consumedMessages: KafkaMessage[] = [];
+  private consumedMessages: KafkaMessage[] = [];
   private latestConsumedOffset = -1n;
 
   private topics: Fixtures['topics'] = [];
@@ -75,7 +77,7 @@ export class KafkajsTestHelper {
         multiplier: 0.5,
         retries: 2,
       },
-      logLevel: logLevel.ERROR,
+      logLevel: logLevel.NOTHING,
     });
 
     this.kafkaAdmin = this.kafka.admin({
@@ -102,6 +104,7 @@ export class KafkajsTestHelper {
   }
 
   async setUp(fixtures: Fixtures): Promise<void> {
+    console.time('SET_UP_DURATION');
     this.schemaRegistry = new SchemaRegistry(
       {
         host: 'http://localhost:8081',
@@ -121,9 +124,12 @@ export class KafkajsTestHelper {
 
     await this.createTopics(fixtures.topics);
     await this.registerSchemas(fixtures.confluentSchemas);
+
     await Promise.all([this.setupConsumer(), this.setupProducer()]);
 
     this.topics = fixtures.topics;
+
+    console.timeEnd('SET_UP_DURATION');
   }
 
   async subscribe(topic: string | RegExp, fromBeginning = true): Promise<void> {
@@ -132,7 +138,7 @@ export class KafkajsTestHelper {
     }
 
     await this.consumer.subscribe({ topics: [topic], fromBeginning });
-
+    console.time('SUBSCRIBE_DURATION');
     await this.consumer.run({
       autoCommit: true,
       autoCommitInterval: null,
@@ -141,13 +147,31 @@ export class KafkajsTestHelper {
       partitionsConsumedConcurrently: 1,
       eachBatch: undefined,
       eachMessage: async ({ message }) => {
+        console.log('florian');
         this.consumedMessages.push(message);
         this.latestConsumedOffset = BigInt(message.offset);
       },
     });
+    console.timeEnd('SUBSCRIBE_DURATION');
+  }
+
+  async waitForMessages(expectedCount, timeout = 5000): Promise<void> {
+    await waitForExpect(() => {
+      expect(this.consumedMessages.length).toBe(expectedCount);
+    }, timeout);
+  }
+
+  getMessages(): KafkaMessage[] {
+    return this.consumedMessages;
+  }
+
+  clearMessages(): void {
+    this.consumedMessages = [];
   }
 
   async cleanUp(): Promise<void> {
+    console.time('CLEAN_UP_DURATION');
+
     await Promise.all([this.cleanUpConsumer(), this.cleanUpProducer()]);
 
     await this.deleteSchemas();
@@ -155,7 +179,7 @@ export class KafkajsTestHelper {
 
     await this.kafkaAdmin.disconnect();
 
-    await new Promise(resolve => setTimeout(resolve, 500));
+    console.timeEnd('CLEAN_UP_DURATION');
   }
 
   /* --------------------------------------------------------------------------
@@ -163,24 +187,28 @@ export class KafkajsTestHelper {
    * -------------------------------------------------------------------------- */
 
   private async createTopics(topics: TopicConfig[]): Promise<void> {
-    const created = await this.kafkaAdmin.createTopics({
-      validateOnly: false,
-      waitForLeaders: true,
-      timeout: 1000,
-      topics: topics.map(topicConfig => ({
-        topic: topicConfig.topicName,
-        numPartitions: 1,
-        replicationFactor: 1,
-        replicaAssignment: [],
-        configEntries: topicConfig.configEntries,
-      })),
-    });
+    try {
+      const created = await this.kafkaAdmin.createTopics({
+        validateOnly: false,
+        waitForLeaders: true,
+        timeout: 1000,
+        topics: topics.map(topicConfig => ({
+          topic: topicConfig.topicName,
+          numPartitions: 1,
+          replicationFactor: 1,
+          replicaAssignment: [],
+          configEntries: topicConfig.configEntries,
+        })),
+      });
 
-    if (!created) {
-      throw new Error('One or more topics already exist.');
+      if (!created) {
+        throw new Error('One or more topics already exist.');
+      }
+
+      this.topics = topics;
+    } catch (error) {
+      console.log('flooooo ', error);
     }
-
-    this.topics = topics;
   }
 
   private async registerSchemas(confluentSchemas: ConfluentSchema[]): Promise<void> {
@@ -189,14 +217,18 @@ export class KafkajsTestHelper {
     }
 
     for (const confluentSchema of confluentSchemas) {
-      await this.schemaRegistry.register(
-        {
-          type: confluentSchema.schema.type,
-          schema: JSON.stringify(confluentSchema.schema.schema), // to make typing working
-          references: confluentSchema.schema.references,
-        },
-        confluentSchema.options,
-      );
+      try {
+        await this.schemaRegistry.register(
+          {
+            type: confluentSchema.schema.type,
+            schema: JSON.stringify(confluentSchema.schema.schema), // to make typing working
+            references: confluentSchema.schema.references,
+          },
+          confluentSchema.options,
+        );
+      } catch (error) {
+        console.log('ici ', error);
+      }
     }
 
     this.confluentSchemas = confluentSchemas;
@@ -204,25 +236,92 @@ export class KafkajsTestHelper {
 
   private async setupConsumer(): Promise<void> {
     if (!this.consumer) {
-      this.consumer = this.kafka.consumer({ groupId: 'test-integration-consumer-id' });
+      // 5 ms
+
+      this.consumer = this.kafka.consumer({
+        groupId: 'test-integration-consumer-id',
+
+        //  groupId: string
+        //   partitionAssigners?: PartitionAssigner[]
+        //   metadataMaxAge?: number
+
+        // Le sessionTimeout est la durée maximale qu'un consommateur peut rester inactif
+        // (c'est-à-dire sans envoyer de heartbeats) avant que le coordonateur de groupe (group coordinator)
+        // de Kafka ne considère que ce consommateur est mort ou déconnecté.
+        // Si le coordonnateur ne reçoit pas de heartbeat dans ce délai, il déclenche une rééquilibrage (rebalance) du groupe.
+
+        // Pour des tests où vous souhaitez une détection rapide des consommateurs inactifs afin de stopper rapidement si aucun message n'est produit :
+        sessionTimeout: 1000, // 1 seconde
+        rebalanceTimeout: 1000, // 1 seconde
+        heartbeatInterval: 300, // 300 millisecondes
+        //   maxBytesPerPartition?: number
+
+        // Le consommateur envoie une requête au serveur Kafka pour obtenir des messages.
+        // Si la quantité de données disponibles est inférieure à minBytes, le serveur
+        // attend que davantage de données s'accumulent (maxWaitTimeInMs), jusqu'à atteindre ce seuil, avant de répondre.
+        // Une valeur élevée peut améliorer le débit en réduisant le nombre de requêtes, mais augmente la latence,
+        // car le consommateur attend plus longtemps pour recevoir les données.
+        // minBytes: 1,
+
+        // Quantité maximale de données (en octets) que le consommateur peut recevoir en une seule requête.
+        // Une valeur trop basse peut limiter le débit en restreignant la quantité de données reçues par requête,
+        // tandis qu'une valeur trop élevée peut entraîner une surcharge de mémoire chez le consommateur.
+        // maxBytes: 1024,
+
+        // maxWaitTimeInMs: 50,
+
+        // Set to 1 byte to ensure the consumer processes messages as soon as they are available,
+        // minimizing latency during tests.
+        minBytes: 1,
+        // Limit to 512 bytes to handle small payloads efficiently, preventing excessive memory usage
+        // while accommodating the expected message size in integration tests.
+        maxBytes: 512,
+        // Set to 5 milliseconds to reduce the wait time for message retrieval, ensuring rapid processing
+        // and quick test execution, suitable for scenarios with low message throughput.
+        maxWaitTimeInMs: 5,
+
+        retry: {
+          maxRetryTime: 2000,
+          initialRetryTime: 50,
+          factor: 1.1,
+          multiplier: 0.5,
+          retries: 2,
+        },
+        allowAutoTopicCreation: false,
+        // maxInFlightRequests: prendre le default,
+        //   readUncommitted?: boolean
+        //   rackId?: string
+      });
       await this.consumer.connect();
     }
   }
 
   private async setupProducer(): Promise<void> {
+    // 10 ms
     if (!this.producer) {
-      this.producer = this.kafka.producer({ allowAutoTopicCreation: false });
+      this.producer = this.kafka.producer({
+        allowAutoTopicCreation: false,
+
+        // createPartitioner?: ICustomPartitioner
+        //   retry?: RetryOptions
+        //   metadataMaxAge?: number
+        //   allowAutoTopicCreation?: boolean
+        //   idempotent?: boolean
+        //   transactionalId?: string
+        //   transactionTimeout?: number
+        //   maxInFlightRequests?: number
+      });
       await this.producer.connect();
     }
   }
 
   private async deleteSchemas(): Promise<void> {
-    for (const subject of this.confluentSchemas.reverse()) {
+    for (const { options } of this.confluentSchemas.reverse()) {
       try {
-        await this.axiosInstance.delete(`/subjects/${subject}`);
-        await this.axiosInstance.delete(`/subjects/${subject}?permanent=true`);
+        await this.axiosInstance.delete(`/subjects/${options.subject}`);
+        await this.axiosInstance.delete(`/subjects/${options.subject}?permanent=true`);
       } catch (error) {
-        console.error(`Failed to delete schema subject: ${subject}`, error);
+        console.error(`Failed to delete schema subject: ${options.subject}`, error);
       }
     }
 
